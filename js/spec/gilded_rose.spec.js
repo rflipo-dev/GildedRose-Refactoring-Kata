9c1496f6de21ba59a4a9ca375f5993239e744ff6
Rémi FLipo <rflipo@octo.com>
2020-02-04 09:43:31 +0100

    Gilded rose JS - WIP -

--- js/spec/gilded_rose.spec.js
+++ js/spec/gilded_rose.spec.js
@@ -1,31 +1,248 @@
-'use strict'
+'use strict';
 
-const expect = require('chai').expect
-const {Item, Shop} = require('../src/gilded_rose')
+const expect = require('chai').expect;
+const { Item, Shop, AgedBrieItem } = require('../src/gilded_rose');
 
-describe('Gilded Rose', function () {
-    const items = []
-    items.push(new Item('+5 Dexterity Vest', 10, 20))
-    items.push(new Item('Aged Brie', 2, 0))
-    items.push(new Item('Elixir of the Mongoose', 5, 7))
-    items.push(new Item('Sulfuras, Hand of Ragnaros', 0, 80))
-    items.push(new Item('Sulfuras, Hand of Ragnaros', -1, 80))
-    items.push(new Item('Backstage passes to a TAFKAL80ETC concert', 15, 20))
-    items.push(new Item('Backstage passes to a TAFKAL80ETC concert', 10, 49))
-    items.push(new Item('Backstage passes to a TAFKAL80ETC concert', 5, 49))
-    // this conjured item does not work properly yet
-    items.push(new Item('Conjured Mana Cake', 3, 6))
+describe('when one day pass given i have an item in my shop', () => {
+   const item = 'Elixir of the Mongoose'
+  describe('with a sellin of 10 and a quality of 5', () => {
+    const sellIn = 10;
+    const quality = 5;
+    const items = [new Item(item, sellIn, quality)];
+    const shop = new Shop(items);
+    shop.ageOneDay();
+    it('should lower quality by 1', () => {
+      expect(shop.items[0].quality).to.eq(quality - 1);
+    });
+    it('should lower sellIn by 1', () => {
+      expect(shop.items[0].sellIn).to.eq(sellIn - 1);
+    });
+  });
+  describe('with a sellIn <= 0', () => {
+    describe('with a sellin of 0 and a quality of 5', () => {
+      const sellIn = 0;
+      const quality = 5;
+      const items = [new Item(item, sellIn, quality)];
+      const shop = new Shop(items);
+      shop.ageOneDay();
+      it('should lower quality by 2', () => {
+        expect(shop.items[0].quality).to.eq(quality - 2 * 1);
+      });
+      it('should lower sellIn by 1', () => {
+        expect(shop.items[0].sellIn).to.eq(sellIn - 1);
+      });
+    });
+    describe('with a sellin of -1 and a quality of 5', () => {
+      const sellIn = -1;
+      const quality = 5;
+      const items = [new Item(item, sellIn, quality)];
+      const shop = new Shop(items);
+      shop.ageOneDay();
+      it('should lower quality by 2', () => {
+        expect(shop.items[0].quality).to.eq(quality - 2 * 1);
+      });
+      it('should lower sellIn by 1', () => {
+        expect(shop.items[0].sellIn).to.eq(sellIn - 1);
+      });
+    });
+  })
+  describe('with a sellin of 4 and a quality of 0', () => {
+    const sellIn = 4;
+    const quality = 0;
+    const items = [new Item(item, sellIn, quality)];
+    const shop = new Shop(items);
+    shop.ageOneDay();
+    it('should not lower quality', () => {
+      expect(shop.items[0].quality).to.eq(quality);
+    });
+  });
+ describe('with a quality above 50', () => {
+  describe('with a sellin of 4 and a quality of 55', () => {
+    const sellIn = 4;
+    const quality = 55;
+    const items = [new Item(item, sellIn, quality)];
+    const shop = new Shop(items);
+    shop.ageOneDay();
+    it('bug - should set the quality to 50 (actual result : 54)', () => {
+      expect(shop.items[0].quality).to.eq(54);
+    });
+  });
+ })
+});
 
-    it('should foo', function () {
-        // Given
-        const items = [new Item('foo', 0, 0)]
-        const gildedRose = new Shop(items)
+describe('when one day pass given i have one  "Aged Brie" in my shop', () => {
+  const item = 'Aged Brie'
+  describe('with a sellin of 10 and a quality of 5', () => {
+    const sellIn = 10;
+    const quality = 5;
+    const items = [new Item(item, sellIn, quality)];
+    const shop = new Shop(items);
+    shop.ageOneDay();
+    it('should increase quality by 1', () => {
+      expect(shop.items[0].quality).to.eq(quality + 1);
+    });
+    it('should lower sellIn by 1', () => {
+      expect(shop.items[0].sellIn).to.eq(sellIn - 1);
+    });
+  });
+  describe('with a sellin of -1 and a quality of 5', () => {
+    const sellIn = -1;
+    const quality = 5;
+    const items = [new Item(item, sellIn, quality)];
+    const shop = new Shop(items);
+    shop.ageOneDay();
+    it('should increase quality by 2', () => {
+      expect(shop.items[0].quality).to.eq(quality + 2);
+    });
+    it('should lower sellIn by 1', () => {
+      expect(shop.items[0].sellIn).to.eq(sellIn - 1);
+    });
+  });
+  describe('with a sellin of -1 and a quality of 49', () => {
+    const sellIn = -1;
+    const quality = 49;
+    const items = [new Item(item, sellIn, quality)];
+    const shop = new Shop(items);
+    shop.ageOneDay();
+    it('should increase quality by 1, because quality should not exceed 50', () => {
+      expect(shop.items[0].quality).to.eq(50);
+    });
+    it('should lower sellIn by 1', () => {
+      expect(shop.items[0].sellIn).to.eq(sellIn - 1);
+    });
+  });
+})
 
-        // When
-        const updatedItems = gildedRose.updateQuality()
+describe('when one day pass given i have one  "Sulfuras, Hand of Ragnaros" in my shop', () => {
+  const item = 'Sulfuras, Hand of Ragnaros'
+  describe('with a sellin of 10 and a quality of 5', () => {
+    const sellIn = 10;
+    const quality = 5;
+    const items = [new Item(item, sellIn, quality)];
+    const shop = new Shop(items);
+    shop.ageOneDay();
+    it('should not alter quality', () => {
+      expect(shop.items[0].quality).to.eq(quality);
+    });
+    it('should not alter sellin', () => {
+      expect(shop.items[0].sellIn).to.eq(sellIn);
+    });
+  });
+  describe('with a sellin of -1 and a quality of -1', () => {
+    const sellIn = -1;
+    const quality = -1;
+    const items = [new Item(item, sellIn, quality)];
+    const shop = new Shop(items);
+    shop.ageOneDay();
+    it('should not alter quality', () => {
+      expect(shop.items[0].quality).to.eq(quality);
+    });
+    it('should not alter sellin', () => {
+      expect(shop.items[0].sellIn).to.eq(sellIn);
+    });
+  });
+})
 
-        // Then
-        expect(items[0].name).to.equal('fixme')
-    })
+describe('when one day pass given i have one "Backstage passes to a TAFKAL80ETC concert" in my shop', () => {
+  const item = 'Backstage passes to a TAFKAL80ETC concert'
 
+  describe('when 10 < sellIn', () => {
+    describe('with a sellin of 12 and a quality of 5', () => {
+      const sellIn = 12;
+      const quality = 5;
+      const items = [new Item(item, sellIn, quality)];
+      const shop = new Shop(items);
+      shop.ageOneDay();
+      it('should increase quality by 1', () => {
+        expect(shop.items[0].quality).to.eq(quality + 1);
+      });
+      it('should lower sellIn by 1', () => {
+        expect(shop.items[0].sellIn).to.eq(sellIn - 1);
+      });
+    });
+  })
+  describe('when 5 < sellIn <= 10', () => {
+    describe('with a sellin of 7 and a quality of 5', () => {
+      const sellIn = 7;
+      const quality = 5;
+      const items = [new Item(item, sellIn, quality)];
+      const shop = new Shop(items);
+      shop.ageOneDay();
+      it('should increase quality by 2', () => {
+        expect(shop.items[0].quality).to.eq(quality + 2 * 1);
+      });
+      it('should lower sellIn by 1', () => {
+        expect(shop.items[0].sellIn).to.eq(sellIn - 1);
+      });
+    });
+  })
+
+  describe('when 0 < sellIn <= 5', () => {
+    describe('with a sellin of 3 and a quality of 5', () => {
+      const sellIn = 3;
+      const quality = 5;
+      const items = [new Item(item, sellIn, quality)];
+      const shop = new Shop(items);
+      shop.ageOneDay();
+      it('should increase quality by 2', () => {
+        expect(shop.items[0].quality).to.eq(quality + 3 * 1);
+      });
+      it('should lower sellIn by 1', () => {
+        expect(shop.items[0].sellIn).to.eq(sellIn - 1);
+      });
+    });
+    describe('with a sellin of 0 and a quality of 5', () => {
+      const sellIn = 0;
+      const quality = 5;
+      const items = [new Item(item, sellIn, quality)];
+      const shop = new Shop(items);
+      shop.ageOneDay();
+      it('should set quality to 0', () => {
+        expect(shop.items[0].quality).to.eq(0);
+      });
+      it('should lower sellIn by 1', () => {
+        expect(shop.items[0].sellIn).to.eq(sellIn - 1);
+      });
+    });
+  })
+});
+
+describe('#AgeBrieItem.ageOneDay', () => {
+  const name = 'Aged Brie'
+  describe('with a sellin of 10 and a quality of 5', () => {
+    const sellIn = 10;
+    const quality = 5;
+    const item = new Item(name, sellIn, quality);
+    const agedBrieItem = AgedBrieItem.ageOneDay(item)
+    it('should increase quality by 1', () => {
+      expect(agedBrieItem.quality).to.eq(quality + 1);
+    });
+    it('should lower sellIn by 1', () => {
+      expect(agedBrieItem.sellIn).to.eq(sellIn - 1);
+    });
+  });
+  describe('with a sellin of -1 and a quality of 5', () => {
+    const sellIn = -1;
+    const quality = 5;
+    const item = new Item(name, sellIn, quality);
+    const agedBrieItem = AgedBrieItem.ageOneDay(item)
+    it('should increase quality by 2', () => {
+      expect(agedBrieItem.quality).to.eq(quality + 2);
+    });
+    it('should lower sellIn by 1', () => {
+      expect(agedBrieItem.sellIn).to.eq(sellIn - 1);
+    });
+  });
+  describe('with a sellin of -1 and a quality of 49', () => {
+    const sellIn = -1;
+    const quality = 49;
+    const item = new Item(name, sellIn, quality);
+    const agedBrieItem = AgedBrieItem.ageOneDay(item)
+    it('should increase quality by 1, because quality should not exceed 50', () => {
+      expect(agedBrieItem.quality).to.eq(50);
+    });
+    it('should lower sellIn by 1', () => {
+      expect(agedBrieItem.sellIn).to.eq(sellIn - 1);
+    });
+  });
 })
--- js/src/gilded_rose.js
+++ js/src/gilded_rose.js
@@ -1,70 +1,82 @@
-'use-strict'
+'use-strict';
 
 class Item {
-    constructor (name, sellIn, quality) {
-        this.name = name
-        this.sellIn = sellIn
-        this.quality = quality
-    }
+  constructor(name, sellIn, quality) {
+    this.name = name;
+    this.sellIn = sellIn;
+    this.quality = quality;
+  }
+}
+
+class AgedBrieItem {
+  static ageOneDay(item) {
+    const MAX_QUALITY = 50;
+    
+    item.sellIn = item.sellIn - 1;
+    item.sellIn < 0
+      ? (item.quality = Math.min(item.quality + 2, MAX_QUALITY))
+      : (item.quality = Math.min(item.quality + 1, MAX_QUALITY));
+
+    return item;
+  }
 }
 
 class Shop {
-    constructor (items = []) {
-        this.items = items
-    }
+  MAX_QUALITY = 50;
+  constructor(items = []) {
+    this.items = items;
+  }
 
-    updateQuality () {
-        for (var i = 0; i < this.items.length; i++) {
-            if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-                if (this.items[i].quality > 0) {
-                    if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                        this.items[i].quality = this.items[i].quality - 1
-                    }
-                }
-            } else {
-                if (this.items[i].quality < 50) {
-                    this.items[i].quality = this.items[i].quality + 1
-                    if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-                        if (this.items[i].sellIn < 11) {
-                            if (this.items[i].quality < 50) {
-                                this.items[i].quality = this.items[i].quality + 1
-                            }
-                        }
-                        if (this.items[i].sellIn < 6) {
-                            if (this.items[i].quality < 50) {
-                                this.items[i].quality = this.items[i].quality + 1
-                            }
-                        }
-                    }
-                }
-            }
-            if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                this.items[i].sellIn = this.items[i].sellIn - 1
-            }
-            if (this.items[i].sellIn < 0) {
-                if (this.items[i].name != 'Aged Brie') {
-                    if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-                        if (this.items[i].quality > 0) {
-                            if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                                this.items[i].quality = this.items[i].quality - 1
-                            }
-                        }
-                    } else {
-                        this.items[i].quality = this.items[i].quality - this.items[i].quality
-                    }
-                } else {
-                    if (this.items[i].quality < 50) {
-                        this.items[i].quality = this.items[i].quality + 1
-                    }
-                }
-            }
+  ageOneDay() {
+    return this.items.map(item => this.ageItemOneDay(item));
+  }
+
+  ageItemOneDay(item) {
+    if (item.name == 'Aged Brie') {
+      return AgedBrieItem.ageOneDay(item);
+    }
+    if (item.name != 'Backstage passes to a TAFKAL80ETC concert') {
+      if (item.quality > 0) {
+        if (item.name != 'Sulfuras, Hand of Ragnaros') {
+          item.quality = item.quality - 1;
         }
-
-        return this.items
+      }
+    } else {
+      if (item.quality < this.MAX_QUALITY) {
+        item.quality = item.quality + 1;
+        if (item.name == 'Backstage passes to a TAFKAL80ETC concert') {
+          if (item.sellIn < 11) {
+            if (item.quality < this.MAX_QUALITY) {
+              item.quality = item.quality + 1;
+            }
+          }
+          if (item.sellIn < 6) {
+            if (item.quality < this.MAX_QUALITY) {
+              item.quality = item.quality + 1;
+            }
+          }
+        }
+      }
     }
+    if (item.name != 'Sulfuras, Hand of Ragnaros') {
+      item.sellIn = item.sellIn - 1;
+    }
+    if (item.sellIn < 0) {
+      if (item.name != 'Backstage passes to a TAFKAL80ETC concert') {
+        if (item.quality > 0) {
+          if (item.name != 'Sulfuras, Hand of Ragnaros') {
+            item.quality = item.quality - 1;
+          }
+        }
+      } else {
+        item.quality = item.quality - item.quality;
+      }
+    }
+  }
 }
 
 module.exports = {
-    Item: Item,
-    Shop: Shop
-}
+  Item: Item,
+  Shop: Shop,
+  AgedBrieItem: AgedBrieItem
+};
